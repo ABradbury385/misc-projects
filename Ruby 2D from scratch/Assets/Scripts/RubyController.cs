ee7873914c3b6389b731acb7adfc3b3c77b7d1c2
amb7316 <56976617+amb7316@users.noreply.github.com>
2020-09-18 00:02:14 -0400

    Added collision objects to game object

--- Ruby 2D from scratch/Assets/Scripts/RubyController.cs
+++ Ruby 2D from scratch/Assets/Scripts/RubyController.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");         //gets base Unity input controls (-1, 0, or 1)
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");         //gets base Unity input controls (-1, 0, or 1)
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //fix up the jittery collisions
+        Vector2 position = rigidbody2d.position;
         position.x += 3f * horizontal * Time.deltaTime;       //when using delta time, char. should move 3-4 units/sec, "frame independent"
         position.y += 3f * vertical * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
