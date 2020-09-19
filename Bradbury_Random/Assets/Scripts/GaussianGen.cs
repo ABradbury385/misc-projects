c23d63cb8356c63262c39f9a9e19c64ae2cc6314
amb7316 <56976617+amb7316@users.noreply.github.com>
2020-09-19 17:34:23 -0400

    Leaders are placed in line with random x offset

--- Bradbury_Random/Assets/Scripts/GaussianGen.cs
+++ Bradbury_Random/Assets/Scripts/GaussianGen.cs
@@ -12,26 +12,34 @@ public class GaussianGen : MonoBehaviour
 
     private GameObject[] leadersArray;
     public int numberOfLeaders = 8;
-    //private TerrainData terrainData;
+    private TerrainData terrainData;
 
     // Start is called before the first frame update
     void Start()
     {
         leadersArray = new GameObject[numberOfLeaders];
-        //terrainData = GetComponent<TerrainCollider>().terrainData;
+        terrainData = GetComponent<TerrainCollider>().terrainData;
 
         for(int i = 0; i < numberOfLeaders; i++)
         {
+            //create a new leader and calculate its scale
             leadersArray[i] = Instantiate(myLeaderPrefab);
             float scaleXAndZ = Gaussian(1f, .15f);
-            Debug.Log("X and Z Scale: " + scaleXAndZ);
             float scaleY = Gaussian(1f, .20f);
-            Debug.Log("Y Scale: " + scaleY);
 
             leadersArray[i].transform.localScale = new Vector3(scaleXAndZ, scaleY, scaleXAndZ);
         }
+
+        PlaceLeaders();
     }
 
+    /// <summary>
+    /// Gaussian (float, float).
+    /// Purpose: Creates a Gaussian distribution value for a given mean and standard deviation.
+    /// </summary>
+    /// <param name="mean">Average value of data</param>
+    /// <param name="stdDev">Standard deviation of the data</param>
+    /// <returns>A random value on a Gaussian distribution</returns>
     float Gaussian(float mean, float stdDev)
     {
         float val1 = Random.Range(0f, 1f);
@@ -41,8 +49,22 @@ public class GaussianGen : MonoBehaviour
         return mean + stdDev * gaussValue;
     }
 
+    /// <summary>
+    /// PlaceLeaders()
+    /// Purpose: Places leaders in a line along the z axis with a random horizontal offset at the appropriate height
+    /// </summary>
     void PlaceLeaders()
     {
+        float leaderGap;
+        for(int i = 0; i < numberOfLeaders; i++)
+        {
+            leaderGap = leadersArray[i].transform.localScale.z / 2 + 1;
 
+            float xOffset = Random.Range(-1f, 1f) + leadersArray[i].transform.position.x;
+            float yOffset = leadersArray[i].transform.position.y;
+            float zOffset = (float) i  * leaderGap + leadersArray[i].transform.position.z;
+
+            leadersArray[i].transform.position = new Vector3(xOffset, yOffset, zOffset);
+        }
     }
 }
