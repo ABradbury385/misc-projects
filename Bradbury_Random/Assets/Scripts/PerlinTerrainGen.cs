24edf1bbb9920b4400ab8d9b6926af85e1f43e30
amb7316 <56976617+amb7316@users.noreply.github.com>
2020-09-19 14:59:44 -0400

    Added Perlin Terrrain script and grass texture

--- Bradbury_Random/Assets/Scripts/PerlinTerrainGen.cs
+++ Bradbury_Random/Assets/Scripts/PerlinTerrainGen.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Author: Andrew Bradbury
+//Purpose: Generate random terrain using Perlin Noise 
+//Limitations: 
+
+[RequireComponent(typeof(TerrainCollider))]
 public class PerlinTerrainGen : MonoBehaviour
 {
+    private TerrainData myTerrainData;
+    public int resolution = 129;
+    private Vector3 worldSize;
+    private float[,] heightMapArray;
+
+    [SerializeField]
+    [Range(0f, .99f)]
+    private float perlinIncrement = .015f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTerrainData = gameObject.GetComponent<TerrainCollider>().terrainData;
+        worldSize = new Vector3(200, 50, 200);
+        myTerrainData.size = worldSize;
+        myTerrainData.heightmapResolution = resolution;
+        heightMapArray = new float[resolution, resolution];
+
+        PerlinGen();
+
+        myTerrainData.SetHeights(0, 0, heightMapArray);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Purpose: Fill a height map with values using Perlin Noise
+    /// Limitations:
+    /// </summary>
+    void PerlinGen()
     {
-        
+        float xOff = 0f;
+        for(int i = 0; i < resolution; i++)
+        {
+            float zOff = 10000f;        //start at arbitrary value for variance in x and z value
+            for(int j = 0; j < resolution; j++)
+            {
+                heightMapArray[i, j] = Mathf.PerlinNoise(xOff, zOff);
+                zOff += perlinIncrement;
+            }
+            xOff += perlinIncrement;
+        }
     }
 }
