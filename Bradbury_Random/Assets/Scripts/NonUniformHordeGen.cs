d145c2a8743fe2453c3cdc5af7dcb30067aae298
amb7316 <56976617+amb7316@users.noreply.github.com>
2020-09-20 13:30:53 -0400

    Added comments to horde generation code

--- Bradbury_Random/Assets/Scripts/NonUniformHordeGen.cs
+++ Bradbury_Random/Assets/Scripts/NonUniformHordeGen.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Author: Andrew Bradbury
+//Purpose: Generate and place a horde of gameobjects that are more concentrated in the front
+//and dwindle out towards the back
 public class NonUniformHordeGen : MonoBehaviour
 {
     [SerializeField]
@@ -21,34 +24,41 @@ public class NonUniformHordeGen : MonoBehaviour
 
         for(int i = 0; i < numberInTheHorde; i++)
         {
+            //position along x axis is completely random
             float xOffset = Random.Range(-hordeSize.x, hordeSize.x);
             xOffset += myPrefab.transform.position.x;
 
             float probability = Random.Range(0f, 1f);
 
+            //z position is also random, but there are more gameobejcts toward the front than in the back
             float zOffset;
             if(probability < .5f)
             {
+                //front quarter
                 zOffset = Random.Range(0, .25f * hordeSize.z);
             }
 
             else if(probability < .8f)
             {
+                //second quarter
                 zOffset = Random.Range(.25f * hordeSize.z, .5f * hordeSize.z);
             }
 
             else if(probability < .95f)
             {
+                //third quarter
                 zOffset = Random.Range(.5f *hordeSize.z, .75f * hordeSize.z);
             }
 
             else
             {
+                //back quarter
                 zOffset = Random.Range(.75f * hordeSize.z, hordeSize.z);
             }
-
+            
             zOffset += myPrefab.transform.position.z;
 
+            //y posiiton is based on height of the terrain
             float yOffset = Terrain.activeTerrain.SampleHeight(new Vector3(xOffset, 0, zOffset))
                 + myPrefab.transform.localScale.y / 2;
 
