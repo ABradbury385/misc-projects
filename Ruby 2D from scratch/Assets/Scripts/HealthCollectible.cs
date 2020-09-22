0fa0b5aa83b942b160fefbb21f63f1e19aaa5a11
amb7316 <56976617+amb7316@users.noreply.github.com>
2020-09-22 00:52:13 -0400

    Made a health collectible and accompanying script

---
+++ Ruby 2D from scratch/Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        RubyController controller = otherCollider.GetComponent<RubyController>();
+        if (controller.Health < controller.maxHealth)
+        {
+            if (controller != null)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
--- Ruby 2D from scratch/Assets/Scripts/RubyController.cs
+++ Ruby 2D from scratch/Assets/Scripts/RubyController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+    public float moveSpeed = 3.0f;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
 
+    public int Health { get { return currentHealth; } }
+
     // Start is called before the first frame update
     void Start()
     {
+        currentHealth = maxHealth;
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
@@ -21,13 +28,19 @@ public class RubyController : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         //fix up the jittery collisions
         Vector2 position = rigidbody2d.position;
-        position.x += 3f * horizontal * Time.deltaTime;       //when using delta time, char. should move 3-4 units/sec, "frame independent"
-        position.y += 3f * vertical * Time.deltaTime;
+        position.x += moveSpeed * horizontal * Time.deltaTime;       //when using delta time, char. should move 3-4 units/sec, "frame independent"
+        position.y += moveSpeed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
