091849a119aed4759fce104ebf5c813f4b575a6f
amb7316 <56976617+amb7316@users.noreply.github.com>
2020-09-30 23:51:43 -0400

    Built game!

--- Ruby 2D from scratch/Assets/Scripts/EnemyController.cs
+++ Ruby 2D from scratch/Assets/Scripts/EnemyController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyController : MonoBehaviour
 {
